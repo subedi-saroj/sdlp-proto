862f16dc667743ae7f97361c83388a877bb44f43
David Alexander <dmalexa5@ncsu.edu>
2025-03-25 15:52:46 -0400

    Grayscale scrolling sequencer and test image

--- scrolling_grayscale.py
+++ scrolling_grayscale.py
@@ -3,8 +3,9 @@ from lux4600.projector import Projector
 from lux4600.records import *
 from lux4600.img import Strip
 from PIL import Image
+import time
 
-img = Image.open(r"test\test-grayscale\1920x4320-gs-stripes.bmp")
+img = Image.open(r"test\test-grayscale\1920x6480_gs2_A.bmp")
 
 strip = Strip(img, 0)
 projector = Projector(IP, DATA_PORT, IMAGE_DATA_PORT)
@@ -13,7 +14,7 @@ projector.check_connection()
 
 projector.send_strip(strip)
 
-sequencer = seq.Sequencer(r"test\test-seq\8bit-1080.txt", 1440)
+sequencer = seq.Sequencer(r"test\test-seq\1920x6480_gs2_scroll.txt", 1440)
 
 projector.send_sequencer(sequencer.packets)
 projector.check_sequencer_error()
@@ -22,3 +23,5 @@ projector.send_strip(strip)
 
 
 projector.start_sequencer()
+input("Press Enter to stop the sequencer...")
+projector.stop_sequencer()
