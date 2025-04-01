7388f7344a3910eec25302e5b9349b73ba522dec
David Alexander <dmalexa5@ncsu.edu>
2025-04-01 15:50:53 -0400

    Minor changes, commenting, etc.

--- scrolling_grayscale.py
+++ scrolling_grayscale.py
@@ -5,15 +5,17 @@ from lux4600.img import Strip
 from PIL import Image
 import time
 
-f1 = r"test\test-grayscale\1920x4320_gs4_A_stitched.bmp"
-f2 = r"test\test-grayscale\1920x6480_gs2_A.bmp"
-img = Image.open(f1)
+# Stitched image produced using boilerplate code in lux4600\grayscale.py
+stitched_image = r"test\test-grayscale\1920x4320_gs4_A_stitched.bmp"
+img = Image.open(stitched_image)
 
 strip = Strip(img, 0)
 projector = Projector(IP, DATA_PORT, IMAGE_DATA_PORT)
 
 projector.check_connection()
 
+# Comment this out if image has already been uploaded since 
+# the projector was last powered on to save time.
 projector.send_strip(strip)
 
 sequencer = seq.Sequencer(r"test\test-seq\1920x4320_gs4_scroll.txt", 1440)
@@ -21,10 +23,13 @@ sequencer = seq.Sequencer(r"test\test-seq\1920x4320_gs4_scroll.txt", 1440)
 projector.send_sequencer(sequencer)
 projector.check_sequencer_error()
 
-# Show a horizantally mirrored strip
-mirror = strip.image.transpose(Image.FLIP_LEFT_RIGHT)
+# Show a horizantally mirrored strip of the underlying image
+# This is, visually, what should be projected
+mirror = Image.open(r'test\test-grayscale\1920x4320_gs4_A.bmp')
+mirror = mirror.transpose(Image.FLIP_LEFT_RIGHT)
 mirror.show()
 
+# Run the sequencer to start the scrolling effect
 projector.start_sequencer()
 input("Press Enter to stop the sequencer...")
 projector.stop_sequencer()
